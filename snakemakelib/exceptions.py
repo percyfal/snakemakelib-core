d7231d290f06099dd275c2e94b597ab24732dc9b
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-15 09:08:09 +0200

    Update exceptions and add find_files to utils

--- snakemakelib/exceptions.py
+++ snakemakelib/exceptions.py
@@ -1,21 +1,7 @@
 # Copyright (C) 2015 by Per Unneberg
 
-
-class NotInstalledError(Exception):
-    """Error thrown if program/command/application cannot be found in path
-
-    Args:
-      msg (str): String described by exception
-      code (int, optional): Error code, defaults to 2.
-
-    """
-    def __init__(self, msg, code=2):
-        self.msg = msg
-        self.code = code
-
-
-class SamplesException(Exception):
-    """Error thrown if samples missing or wrong number.
+class SnakemakelibException(Exception):
+    """Base exception class
 
     Args:
       msg (str): String described by exception
@@ -26,15 +12,14 @@ class SamplesException(Exception):
         self.msg = msg
         self.code = code
 
+class NotInstalledError(SnakemakelibException):
+    """Error thrown if program/command/application cannot be found in path"""
 
-class OutputFilesException(Exception):
-    """Error thrown if outputfiles missing or wrong number.
+class SamplesException(SnakemakelibException):
+    """Error thrown if samples missing or wrong number."""
 
-    Args:
-      msg (str): String described by exception
-      code (int, optional): Error code, defaults to 2.
+class OutputFilesException(SnakemakelibException):
+    """Error thrown if outputfiles missing or wrong number."""
 
-    """
-    def __init__(self, msg, code=2):
-        self.msg = msg
-        self.code = code
+class DeprecatedException(SnakemakelibException):
+    """Error thrown for deprecated functions."""
--- snakemakelib/utils.py
+++ snakemakelib/utils.py
@@ -38,3 +38,108 @@ def safe_makedir(dname):
     else:
         smllogger.warning("Directory {} already exists; not making directory".format(dname))
     return dname
+
+
+
+def find_files(regexp, path=os.curdir, search=False, limit=None):
+    """Find files in path that comply with a regular expression.
+
+    Args:
+      regexp (RegexpDict | str): regular expression object of class
+                               <RegexpDict> or <str>
+      path (str):   path to search
+      search (bool): use re.search instead of re.match for pattern matching
+      limit (dict): dictionary where keys correspond to regular expression
+             grouping labels and values are lists that limit the
+             returned pattern
+
+    Returns:
+      flist: list of file names, prepended with root path
+    """
+    if isinstance(regexp, RegexpDict):
+        r = regexp.re
+    else:
+        if not regexp:
+            return []
+        r = re.compile(regexp)
+    if not limit is None and any(k not in r.groupindex.keys() for k in limit.keys()):
+        smllogger.warning("""Some limit keys '{}' not in regexp
+        groupindex '{}'; disregarding limit option for these
+        keys""".format(list(limit.keys()), list(r.groupindex.keys())))
+    re_fn = r.search if search else r.match
+    flist = []
+    for root, dirs, files in os.walk(path):
+        for x in files:
+            m = re_fn(x)
+            if m is None:
+                continue
+            if limit:
+                if any([m.group(k) in limit[k] for k in limit.keys() if k in m.groupdict().keys()]):
+                    flist += [os.path.join(root, x)]
+            else:
+                flist += [os.path.join(root, x)]
+    return sorted(flist)
+
+def dict_to_R(d, as_string=True):
+    """Translate a python dictionary to an R option string
+
+    Args:
+      d: dictionary
+
+    Returns:
+      A string representing an option string of the dictionary in R
+    """
+    outlist = []
+    for (k, v) in d.items():
+        if isinstance(v, list):
+            if isinstance(v[0], int):
+                outlist.append("{k} = c(".format(k=k) + ",".join("{vv}".format(vv=vv) for vv in v) + ")")
+            else:
+                outlist.append("{k} = c(".format(k=k) + ",".join("'{vv}'".format(vv=vv) for vv in v) + ")")     
+        elif isinstance(v, dict):
+            outlist.append("{k} = c(".format(k=k) + ",".join("{kk}={vv}".format(kk=kk, vv=vv) for (kk,vv) in v.items()) + ")")
+        elif v is True:
+            outlist.append("{k}=TRUE".format(k=k))
+        elif v is False:
+            outlist.append("{k}=FALSE".format(k=k))
+        elif v is None:
+            outlist.append("{k}=NULL".format(k=k))
+        elif isinstance(v, int):
+            outlist.append("{k}={v}".format(k=k, v=v))
+        elif isinstance(v, float):
+            outlist.append("{k}={v}".format(k=k, v=v))
+        else:
+            outlist.append("{k}='{v}'".format(k=k, v=v))
+    return ",".join(outlist)
+
+def dict_to_Rdict(d, as_string=True):
+    """Translate a python dictionary to a dict with R-compatible entries
+
+    Args:
+      d: dictionary
+
+    Returns:
+      A dictionare where values comply with R
+    """
+    dout = {}
+    for (k, v) in d.items():
+        if isinstance(v, list):
+            if isinstance(v[0], int):
+                dout[k] = "c(" + ",".join("{vv}".format(vv=vv) for vv in v) + ")"
+            else:
+                dout[k] = "c(" + ",".join("'{vv}'".format(vv=vv) for vv in v) + ")"
+        elif isinstance(v, dict):
+            dout[k] = "c(" + ",".join("{kk}={vv}".format(kk=kk, vv=vv) for (kk,vv) in v.items()) + ")"
+        elif v is True:
+            dout[k] = "TRUE"
+        elif v is False:
+            dout[k] = "FALSE"
+        elif v is None:
+            dout[k] = "NULL"
+        elif isinstance(v, int):
+            dout[k] = v
+        elif isinstance(v, float):
+            dout[k] = v
+        else:
+            dout[k] = '{v}'.format(v=v)
+    return dout
