aefd42f294b89ae4ac7d1f481ec2958360e7e539
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-16 15:21:45 +0200

    Add versioneer version function

--- snakemakelib/__init__.py
+++ snakemakelib/__init__.py
@@ -1,5 +1,13 @@
-# Copyright (C) 2015 by Per Unneberg
+# Copyright (c) 2013 Per Unneberg
 import os
+
+__import__('pkg_resources').declare_namespace(__name__)
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+
 
 SNAKEMAKELIB_PATH = os.path.dirname(__file__)
 SNAKEMAKELIB_TEMPLATES_PATH = os.path.join(SNAKEMAKELIB_PATH, "_templates")
