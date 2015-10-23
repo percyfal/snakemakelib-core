eee158aba59cdf657b2c9847cb7a4fa73173f2ee
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-23 11:03:36 +0200

    Add code and tests for qualimap, picard

--- snakemakelib/odo/cutadapt.py
+++ snakemakelib/odo/cutadapt.py
@@ -3,6 +3,7 @@ import re
 from blaze import resource, DataFrame
 import pytest
 import numpy as np
+import pandas as pd
 
 # Potentially add regexp for adapter sections as these are repetitive
 adapter_re = re.compile(r'''
@@ -13,8 +14,8 @@ adapter_re = re.compile(r'''
 re_trim = re.compile(r'(\([0-9.]+%\)|,| |bp)')
 
 def _split_x(x, delim=":"):
-    y = x.strip().split(":")
-    return [y[0], int(re_trim.sub("", y[1]))]
+    y = x.strip().split(delim)
+    return [y[0], re_trim.sub("", y[1])]
 
 # For now only return the summary section
 @resource.register('.+\.cutadapt_metrics')
@@ -24,5 +25,6 @@ def resource_cutadapt_metrics(uri, **kwargs):
     sections = re.split("\n===.*===\n", data)
     df = DataFrame.from_records([_split_x(x) for x in sections[1].split("\n") if x],
                                 index="statistic", columns=["statistic", "value"])
+    df["value"] = pd.to_numeric(df["value"])
     return df
 
--- snakemakelib/odo/qualimap.py
+++ snakemakelib/odo/qualimap.py
@@ -1,28 +1,37 @@
 # Copyright (C) 2015 by Per Unneberg
 import re
-from . import resource, DataFrame
+from blaze import resource, DataFrame
+import pandas as pd
 
 COVERAGE_PER_CONTIG_COLUMNS = ["chr", "chrlen", "mapped_bases",
                                "mean_coverage", "sd"]
 
-# resource.register apparently must have a wildcard prefix to work
-# Set priority to override default text resources
+
+re_trim = re.compile(r'(,|\s+bp.*$|\s+\(.*%\)|%)')
+
+def _split_x(x, delim=" = "):
+    y = x.strip().split(delim)
+    return [y[0], re_trim.sub("", y[1])]
+
 @resource.register('.*genome_results.txt', priority=20)
 def resource_genome_results(uri, **kwargs):
     with open(uri) as fh:
-        lines = [re.sub(r'(,|\s+bp.*$|\s+\(.*%\))', r'', x.strip()) for x in fh.readlines() if x.strip() != ""]
-    indices = list((i for i, val in enumerate(lines)
-                    if val.startswith(">>>"))) + [len(lines)]
-    headings = ["_".join(lines[i].split(" ")[1:]) for i in indices[:-1]]
-    data = {}
-    for i in range(len(indices) - 1):
-        ll = lines[(indices[i]+1):indices[i+1]]
-        if headings[i] in ["Coverage_per_contig"]:
-            data[headings[i]] =  DataFrame([x.split("\t") for x in ll])
-            data[headings[i]].columns = [COVERAGE_PER_CONTIG_COLUMNS]
-        elif headings[i] in ["Coverage"]:
+        data = "".join(fh)
+    sections = re.split(">+\s+[a-zA-Z ]+", data)
+    section_names = ["Header"] + [re.sub(" ", "_", x) for x in re.findall(">+\s+([a-zA-Z ]+)", data)]
+    d = dict()
+    for h, sec in zip(section_names, sections):
+        if h == "Coverage_per_contig":
+            d[h] = DataFrame.from_records([x.strip().split("\t") for x in sec.split("\n") if x],
+                                          columns=COVERAGE_PER_CONTIG_COLUMNS,
+                                          index="chr")
+            d[h] = d[h].apply(pd.to_numeric)
+        elif h in ["Coverage", "Header"]:
             pass
         else:
-            data[headings[i]] = DataFrame([x.split(" = ") for x in ll])
-            data[headings[i]].columns = ['statistic', 'value']
-    return data
+            d[h] = DataFrame.from_records([_split_x(x) for x in sec.split("\n") if x],
+                                          columns=["statistic", "value"],
+                                          index="statistic")
+            if not h in ["Input"]:
+                d[h] = d[h].apply(pd.to_numeric)
+    return d
