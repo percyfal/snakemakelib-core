3ecf5bff503ccd1480b4d1a518304789e233b38a
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-28 00:29:15 +0100

    Add statistics and resource files

---
+++ snakemakelib/static/js/snakemakelib.js
@@ -0,0 +1,57 @@
+var pca_callback = function(cb_obj, source) {
+    var data = source.get('data');
+    var active = cb_obj.get('active');
+    // If radio button group need index
+    // var label = cb_obj.get('label')[active]
+    var label = cb_obj.get('label');
+    var Paired = {
+	3	: [ "#A6CEE3","#1F78B4","#B2DF8A"],
+	4	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C"],
+	5	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99"],
+	6	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C"],
+	7	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F"],
+	8	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00"] ,
+	9	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6"],
+	10	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A"],
+	11	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A","#FFFF99"],
+	12	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A","#FFFF99", "#B15928"]};
+    var colormap = {};
+    // Comment out if(!active) if radiobuttongroup
+    if (!active) {
+	if (typeof(data[label][0]) != "string") {
+	    var minsize = Math.log(Math.min.apply(null, data[label]))/Math.LN2;
+	    var maxsize = Math.log(Math.max.apply(null, data[label]))/Math.LN2;
+	    for (i = 0; i < data['sample'].length; i++) {
+		data['size'][i] = 10 + (Math.log(data[label][i])/Math.LN2 - minsize) / (maxsize - minsize) * 20;
+	    }
+	} else {
+	    var j = 0;
+	    // Simple categorical grouping
+	    for (i = 0; i < data['sample'].length; i++) {
+		if (data[label][i] in colormap) {
+		} else {
+		    colormap[data[label][i]] = j;
+		    j++;
+		}
+	    }
+	    var nfac = Object.keys(colormap).length;
+	    if (nfac > 12) {
+		nfac = 12;
+	    } 
+	    if (nfac < 3) {
+		nfac = 3;
+	    }
+	    var colors = Paired[nfac];
+	    for (i = 0; i < data[label].length; i++) {
+		data['color'][i] = colors[colormap[data[label][i]]]
+	    }
+	}
+    } else {
+	if (typeof(data[label][i] != "string")) {
+	    for (i = 0; i < data['sample'].length; i++) {
+		data['size'][i] = 10;
+	    }
+	}
+    }
+    source.trigger('change');
+}
---
+++ snakemakelib/statistics/sklearn.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2015 by Per Unneberg
+"""sklearn machine learning"""
+from sklearn.decomposition import PCA
+import pandas as pd
+
+def pca(expr, **kwargs):
+    """scrnaseq pca - run pca
+
+    Args:
+      expr (DataFrame): pandas data frame with expression values, rows
+      correspond to samples/experimental units, columns to genes
+      kwargs: keyword arguments
+
+    Returns:
+      pcaobj (PCA): PCA model fitted to expr
+    """
+    pcaobj = PCA(n_components=kwargs.get("n_components", 10))
+    pcaobj.fit(expr)
+    return pcaobj
+
+def pca_results(pcaobj, expr, metadata=None, **kwargs):
+    """Generate output file on which to base pca plots
+    
+    Args:
+      pcaobj (PCA): pca model
+      expr (DataFrame): pandas data frame with expression values
+      metadata (DataFrame): sample metadata
+
+    Returns:
+      pcares (DataFrame): pca results concatenated,
+      loadings (DataFrame): pca loadings of top 10 units for each component
+    """
+    pcares = pd.DataFrame(pcaobj.fit(expr).transform(expr))
+    if not expr.index.name is None:
+        pcares.index = expr.index
+    if not metadata is None:
+        md = pd.read_csv(metadata, index_col=0)
+        pcares = pcares.join(md)
+    return pcares
