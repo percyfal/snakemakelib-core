1c44e000ee39e12cd7f20b97783bad5d7b49d1b3
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-28 00:31:05 +0100

    Add item parameter

--- snakemakelib/static/js/snakemakelib.js
+++ snakemakelib/static/js/snakemakelib.js
@@ -1,10 +1,4 @@
-var pca_callback = function(cb_obj, source) {
-    var data = source.get('data');
-    var active = cb_obj.get('active');
-    // If radio button group need index
-    // var label = cb_obj.get('label')[active]
-    var label = cb_obj.get('label');
-    var Paired = {
+var colorbrewer = {Paired: {
 	3	: [ "#A6CEE3","#1F78B4","#B2DF8A"],
 	4	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C"],
 	5	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99"],
@@ -14,20 +8,28 @@ var pca_callback = function(cb_obj, source) {
 	9	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6"],
 	10	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A"],
 	11	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A","#FFFF99"],
-	12	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A","#FFFF99", "#B15928"]};
+    12	: [ "#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A","#FFFF99", "#B15928"]}};
+
+var pca_callback;
+pca_callback = function(source, cb_obj, item) {
+    var data = source.get('data');
+    var active = cb_obj.get('active');
+    // If radio button group need index
+    // var label = cb_obj.get('label')[active]
+    var label = cb_obj.get('label');
     var colormap = {};
     // Comment out if(!active) if radiobuttongroup
     if (!active) {
 	if (typeof(data[label][0]) != "string") {
 	    var minsize = Math.log(Math.min.apply(null, data[label]))/Math.LN2;
 	    var maxsize = Math.log(Math.max.apply(null, data[label]))/Math.LN2;
-	    for (i = 0; i < data['sample'].length; i++) {
+	    for (i = 0; i < data[item].length; i++) {
 		data['size'][i] = 10 + (Math.log(data[label][i])/Math.LN2 - minsize) / (maxsize - minsize) * 20;
 	    }
 	} else {
 	    var j = 0;
 	    // Simple categorical grouping
-	    for (i = 0; i < data['sample'].length; i++) {
+	    for (i = 0; i < data[item].length; i++) {
 		if (data[label][i] in colormap) {
 		} else {
 		    colormap[data[label][i]] = j;
@@ -41,17 +43,29 @@ var pca_callback = function(cb_obj, source) {
 	    if (nfac < 3) {
 		nfac = 3;
 	    }
-	    var colors = Paired[nfac];
+	    var colors = colorbrewer['Paired'][nfac];
 	    for (i = 0; i < data[label].length; i++) {
 		data['color'][i] = colors[colormap[data[label][i]]]
 	    }
 	}
     } else {
 	if (typeof(data[label][i] != "string")) {
-	    for (i = 0; i < data['sample'].length; i++) {
+	    for (i = 0; i < data[item].length; i++) {
 		data['size'][i] = 10;
 	    }
 	}
     }
     source.trigger('change');
-}
+};
+
+var pca_component;
+pca_component = function(source, cb_obj, component) {
+    var myRe = /^([0-9]+)/;
+    var data = source.get('data');
+    var value = myRe.exec(String(cb_obj.get('value')))[1]
+    var component = data[component]
+    for (i = 0; i < component.length; i++) {
+        component[i] = data[value][i]
+    }
+    source.trigger('change');
+};
