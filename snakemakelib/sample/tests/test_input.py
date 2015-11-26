5b59203d097ddb773f0766e1744050c8ccff9732
Per Unneberg <per.unneberg@scilifelab.se>
2015-11-26 20:53:19 +0100

    Update input tests and return concat_groupdict

--- snakemakelib/sample/tests/test_input.py
+++ snakemakelib/sample/tests/test_input.py
@@ -2,7 +2,7 @@
 # pylint: disable=R0904, C0301, C0103
 import csv
 import pytest
-from ..input import parse_sampleinfo, samples_from_input_files
+from ..input import _parse_sampleinfo, _samples_from_input_files
 from snakemakelib.sample.organization import config, illumina_scilife
 
 
@@ -20,16 +20,16 @@ def illumina_scilife_files():
 
 class TestParseSampleinfo:
     def test_parse_sampleinfo(self):
-        samples = parse_sampleinfo(sampleinfo=csv.DictReader(['SM\n', 'Sample1\n', 'Sample2\n']))
+        samples = _parse_sampleinfo(sampleinfo=csv.DictReader(['SM\n', 'Sample1\n', 'Sample2\n']))
         assert samples == [{'SM':'Sample1'}, {'SM':'Sample2'}]
 
     def test_parse_sampleinfo_samplecolumn(self):
-        samples = parse_sampleinfo(sampleinfo=csv.DictReader(['Sample\n', 'Sample1\n', 'Sample2\n']),
+        samples = _parse_sampleinfo(sampleinfo=csv.DictReader(['Sample\n', 'Sample1\n', 'Sample2\n']),
                                    sample_column_map={'Sample': 'SM', 'Platform': 'PL'})
         assert samples == [{'SM':'Sample1'}, {'SM':'Sample2'}]
 
     def test_parse_sampleinfo_wrong_samplecolumn(self):
-        samples = parse_sampleinfo(sampleinfo=csv.DictReader(['Sample\n', 'Sample1\n', 'Sample2\n']),
+        samples = _parse_sampleinfo(sampleinfo=csv.DictReader(['Sample\n', 'Sample1\n', 'Sample2\n']),
                                    sample_column_map={'SMM': 'SM', 'Platform': 'PL'})
         assert samples == [{'Sample':'Sample1'}, {'Sample':'Sample2'}]
 
@@ -38,6 +38,6 @@ class TestSamplesFromInputFiles:
     def test_samples_from_input_files(self, illumina_scilife_files, mocker):
         mock_find = mocker.patch('snakemakelib.sample.input.find_files')
         mock_find.return_value = illumina_scilife_files
-        samples = samples_from_input_files(config['settings']['sample_organization'].run_id_re)
-        assert samples[0] == {'SM': 'P001_102', 'DT': '120924', 'PU2': '2', 'PU': 'AC003CCCXX_2', 'PU1': 'AC003CCCXX', 'PATH': 'P001_102/120924_AC003CCCXX'}
+        samples = _samples_from_input_files(config['settings']['sample_organization'].run_id_re)
+        assert samples[0] == {'SM': 'P001_102', 'DT': '120924', 'PU2': '2', 'PU': 'AC003CCCXX_2', 'PU1': 'AC003CCCXX'}
 
