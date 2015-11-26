be3d0fffbafeb403c02ca6385605a12d907a8d00
Per Unneberg <per.unneberg@scilifelab.se>
2015-11-26 10:53:06 +0100

    Add odo.pandas for concatenating data frames
    Also add annotate_by_uri

---
+++ snakemakelib/odo/pandas.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2015 by Per Unneberg
+import pandas as pd
+from blaze import append, DataFrame
+from odo.backends import pandas
+
+
+@append.register(DataFrame, DataFrame)
+def append_dataframe_to_dataframe(tgt, src, **kw):
+    tgt = pd.concat([tgt, src])
+    return tgt
+
+
+def annotate_by_uri(df, uri, **kwargs):
+    if 'annotation_fn' in kwargs:
+        func = kwargs.pop('annotation_fn')
+        return func(df, uri, **kwargs)
+    else:
+        df['uri'] = uri
+    return df
