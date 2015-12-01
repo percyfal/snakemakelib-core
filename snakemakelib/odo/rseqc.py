e1e2d60e7e176611bbf4fe8996e1f15ebf9ea933
Per Unneberg <per.unneberg@scilifelab.se>
2015-12-01 23:49:32 +0100

    Add annotate_by_uri

--- snakemakelib/odo/rseqc.py
+++ snakemakelib/odo/rseqc.py
@@ -3,8 +3,10 @@ import re
 from glob import glob
 import pandas as pd
 from blaze import resource, DataFrame
+from .pandas import annotate_by_uri
 
 @resource.register('.+read_distribution.txt', priority=20)
+@annotate_by_uri
 def resource_rseqc_read_distribution(uri, **kwargs):
     df = pd.read_table(uri, skiprows=list(range(0,7)) + [18], sep="[ ]+", engine="python")
     df = df.set_index("Group")
@@ -12,6 +14,7 @@ def resource_rseqc_read_distribution(uri, **kwargs):
 
 
 @resource.register('.+geneBody_coverage.geneBodyCoverage.txt', priority=20)
+@annotate_by_uri
 def resource_rseqc_genebody_coverage(uri, **kwargs):
     df = pd.read_table(uri, header=0)
     return df
