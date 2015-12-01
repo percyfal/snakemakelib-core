cbcec1c5a7e011ea8ec589acf794bbd41d7ace8a
Per Unneberg <per.unneberg@scilifelab.se>
2015-12-01 10:32:24 +0100

    Simplify make_targets, update release notes

--- snakemakelib/io.py
+++ snakemakelib/io.py
@@ -4,7 +4,6 @@ import re
 from itertools import groupby
 import snakemake.workflow
 from snakemake.io import _wildcard_regex, regex
-from snakemakelib.sample.regexp import RegexpDict
 from snakemakelib.log import LoggerManager
 
 smllogger = LoggerManager().getLogger(__name__)
@@ -13,24 +12,22 @@ class MissingRequiredKeyException(Exception):
     """Exception if required key is missing"""
 
 
-def make_targets(tgt_re, samples, target_suffix=""):
+def make_targets(tgt_re, samples):
     """Make targets
     
-    Create target names based on the target regular expression and a
-    target suffix.
+    Create target names based on the target regular expression.
 
     Args:
-      tgt_re (RegexpDict): RegexpDict object corresponding to the target
+      tgt_re (IOTarget): IOTarget object corresponding to the target
                            regular expression
 
       samples (list): list of dicts where each dict is an annotated
                       sample. The keys correspond to read group labels.
-      target_suffix (str): target suffix to add to target regexp
 
     Returns:
       targets (list): list of target names
     """
-    tgts = list(set(tgt_re.fmt.format(**unit) + target_suffix for unit in samples))
+    tgts = list(set(tgt_re.fmt.format(**unit) for unit in samples))
     return tgts
 
 
