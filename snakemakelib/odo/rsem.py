cdb8329c7520d9deeba19656f3252331fc357641
Per Unneberg <per.unneberg@scilifelab.se>
2015-12-01 18:57:53 +0100

    Add annotate_by_uri

--- snakemakelib/odo/rsem.py
+++ snakemakelib/odo/rsem.py
@@ -1,16 +1,19 @@
 # Copyright (C) 2015 by Per Unneberg
 from blaze import resource
 import pandas as pd
+from .pandas import annotate_by_uri
 
 @resource.register('.+\.genes\.results')
-def resource_genes_results(uri):
+@annotate_by_uri
+def resource_genes_results(uri, **kwargs):
     with open(uri):
         data = pd.read_csv(uri, sep="\t", header=0, comment="#",
                            index_col=["gene_id"])
     return data
 
 @resource.register('.+\.isoforms\.results')
-def resource_isoforms_results(uri):
+@annotate_by_uri
+def resource_isoforms_results(uri, **kwargs):
     with open(uri):
         data = pd.read_csv(uri, sep="\t", header=0, comment="#",
                            index_col=["transcript_id"])
