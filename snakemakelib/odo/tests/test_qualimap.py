04cd2ca00b2e38c4687deef76b7bfc52e1a0c85b
Per Unneberg <per.unneberg@scilifelab.se>
2016-02-01 17:18:16 +0100

    Add support for qualimap v2.1.3

--- snakemakelib/odo/tests/test_qualimap.py
+++ snakemakelib/odo/tests/test_qualimap.py
@@ -87,7 +87,97 @@ def qualimap_data(tmpdir_factory):
 """)
     return fn
 
+
+@pytest.fixture(scope="module")
+def qualimap_data_2_1_3(tmpdir_factory):
+    fn = tmpdir_factory.mktemp('test.bam.qualimap').join('genome_results.txt')
+    fn.write("""BamQC report
+-----------------------------------
+
+>>>>>>> Input
+
+     bam file = test.bam
+     outfile = /home/peru/dev/snakemake-rules/snakemake_rules/tests/data/test_stats/genome_results.txt
+
+
+>>>>>>> Reference
+
+     number of bases = 2,000,000 bp
+     number of contigs = 1
+
+
+>>>>>>> Globals
+
+     number of windows = 400
+
+     number of reads = 50
+     number of mapped reads = 50 (100%)
+
+     number of mapped paired reads (first in pair) = 25
+     number of mapped paired reads (second in pair) = 25
+     number of mapped paired reads (both in pair) = 50
+     number of mapped paired reads (singletons) = 0
+
+     number of mapped bases = 3,795 bp
+     number of sequenced bases = 3,793 bp
+     number of aligned bases = 0 bp
+     number of duplicated reads (estimated) = 0
+     duplication rate = 0%
+
+
+>>>>>>> Insert size
+
+     mean insert size = 173.32
+     std insert size = 76.98
+     median insert size = 155
+
+
+>>>>>>> Mapping quality
+     mean mapping quality = 0.3
+
+
+>>>>>>> ACTG content
+
+     number of A's = 971 bp (25.6%)
+     number of C's = 999 bp (26.34%)
+     number of T's = 868 bp (22.88%)
+     number of G's = 955 bp (25.18%)
+     number of N's = 0 bp (0%)
+
+     GC percentage = 51.52%
+
+
+>>>>>>> Mismatches and indels
+
+    general error rate = 0.01
+    number of mismatches = 26
+
+
+>>>>>>> Coverage
+
+     mean coverageData = 0X
+     std coverageData = 0.16X
+
+     There is a 0.03% of reference with a coverageData >= 1X
+     There is a 0.02% of reference with a coverageData >= 2X
+     There is a 0.02% of reference with a coverageData >= 3X
+
+
+>>>>>>> Coverage per contig
+
+        chr11   2000000 3795    0.0018975       0.15562423747202747
+
+""")
+    return fn
+
+
 def test_qualimap(qualimap_data):
     df = odo(str(qualimap_data), DataFrame, key='Coverage_per_contig')
     assert list(df.columns) == ['chrlen', 'mapped_bases', 'mean_coverage', 'sd']
     assert list(df.index) == ['chr10', 'chr11']
+
+
+def test_qualimap_2_1_3(qualimap_data_2_1_3):
+    df = odo(str(qualimap_data_2_1_3), DataFrame, key='Coverage_per_contig')
+    assert list(df.columns) == ['chrlen', 'mapped_bases', 'mean_coverage', 'sd']
+    assert list(df.index) == ['chr11']
